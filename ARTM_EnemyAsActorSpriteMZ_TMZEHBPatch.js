2c0e0818d5047a500df5d6094baf4cb542cf180c
ArtemisJP <enjeriza@yahoo.co.jp>
2021-06-27 20:49:20 +0900

    Update ARTM_EnemyAsActorSpriteMZ_TMZEHBPatch.js
    v1.0.1 位置指定TOP時の競合漏れを修正

--- ARTM_EnemyAsActorSpriteMZ_TMZEHBPatch.js
+++ ARTM_EnemyAsActorSpriteMZ_TMZEHBPatch.js
@@ -6,6 +6,7 @@
 // ===================================================
 // [Version]
 // 1.0.0 初版
+// 1.0.1 位置指定TOP時の競合漏れを修正
 // =================================================================
 /*:ja
  * @target MZ
@@ -25,30 +26,37 @@
 
 (() => {
 
+    //const Torigoya = (window.Torigoya = window.Torigoya || {});
+
     //-----------------------------------------------------------------------------
     // *Patch for TorigoyaMZ_EnemyHpBar
     //
+    const _Sprite_Battler_initialize = Sprite_Battler.prototype.initialize;
+    Sprite_Battler.prototype.initialize = function(battler) {
+        _Sprite_Battler_initialize.call(this, battler);
+        if (!(battler && battler.asEnemy())) { return; }
+        this._torigoyaEnemyHpBar_gaugeSprite = new Torigoya.EnemyHpBar.Sprite_EnemyHpGauge();
+        this._torigoyaEnemyHpBar_gaugeSprite.anchor.x = 0.5;
+        if (Torigoya.EnemyHpBar.parameter.basePosition === "top") {
+            this._torigoyaEnemyHpBar_gaugeSprite.anchor.y = 1;
+        }
+        this._torigoyaEnemyHpBar_gaugeSprite.opacity = 0;
+        this._torigoyaEnemyHpBar_gaugeSprite.setup(battler, "hp");
+        this._torigoyaEnemyHpBar_gaugeSprite.scale.x = -1;
+        this.addChild(this._torigoyaEnemyHpBar_gaugeSprite);
+    };
+
     const _Sprite_Actor_update = Sprite_Actor.prototype.update;
     Sprite_Actor.prototype.update = function() {
         _Sprite_Actor_update.call(this);
         const actor = this._actor;
         if (actor && actor.asEnemy()) {
             this.torigoyaEnemyHpBar_updateGaugeSprite();
-            this._torigoyaEnemyHpBar_gaugeSprite.y = -this.height;
+            this._torigoyaEnemyHpBar_gaugeSprite.x *= -1;
+            if (Torigoya.EnemyHpBar.parameter.basePosition === "top") {
+                this._torigoyaEnemyHpBar_gaugeSprite.y += -this.height;
+            }
         }
     }
-
-    const _Sprite_Battler_initialize = Sprite_Battler.prototype.initialize;
-    Sprite_Battler.prototype.initialize = function(battler) {
-        _Sprite_Battler_initialize.call(this, battler);
-        if (!(battler && battler.asEnemy())) { return; }
-        this._torigoyaEnemyHpBar_gaugeSprite = new Torigoya.EnemyHpBar.Sprite_EnemyHpGauge();
-        this._torigoyaEnemyHpBar_gaugeSprite.anchor.y = 1;
-        this._torigoyaEnemyHpBar_gaugeSprite.anchor.x = 0.5;
-        this._torigoyaEnemyHpBar_gaugeSprite.opacity = 0;
-        this._torigoyaEnemyHpBar_gaugeSprite.setup(battler, 'hp');
-        this._torigoyaEnemyHpBar_gaugeSprite.scale.x = -1;
-        this.addChild(this._torigoyaEnemyHpBar_gaugeSprite);
-    };
 
 })();
